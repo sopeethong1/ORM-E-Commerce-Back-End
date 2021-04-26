e32b0d616f9f1aa4fdeeba38998d3d0e783647c6
Sopee Thong <tsopee@gmail.com>
2021-04-25 22:40:57 -0400

    adding cat_name column

--- DEVELOP/models/Category.js
+++ DEVELOP/models/Category.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+//creates Category model
 class Category extends Model {}
 
+//creates columns for Category model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
