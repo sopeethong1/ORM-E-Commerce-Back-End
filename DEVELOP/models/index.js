3b630ebb10f6b7bfaae4a01f4544a3aad44a50fb
Sopee Thong <tsopee@gmail.com>
2021-04-26 00:24:09 -0400

    formatted

--- DEVELOP/models/index.js
+++ DEVELOP/models/index.js
@@ -1,36 +1,35 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category,{
-foreignKey: 'category_id'
-})
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-Category.hasMany(Product,{
-foreignKey: 'category_id',
-onDelete: 'CASCADE'
-})
-
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tag'
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tag",
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: false
+    unique: false,
   },
-  as: 'product_tag'
+  as: "product_tag",
 });
 
 module.exports = {
