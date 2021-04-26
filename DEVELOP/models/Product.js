4c4fc5744178f7af869253f12953113f761dfa03
Sopee Thong <tsopee@gmail.com>
2021-04-25 22:58:17 -0400

    adding cat id and reference key

--- DEVELOP/models/Product.js
+++ DEVELOP/models/Product.js
@@ -1,5 +1,6 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+const { Category } = require('.');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -31,8 +32,14 @@ Product.init(
         defaultValue: 10,
         validate: {
           isNumeric: true
-
     },
+    category_id: {
+      type: DataTypes.INTEGER,
+       references: {
+       model: 'Category',
+       key: 'id',
+     }
+  },
     }
   },
   {
