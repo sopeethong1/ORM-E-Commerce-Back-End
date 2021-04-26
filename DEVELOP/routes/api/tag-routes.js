786247b7c7986ea3da3292b272038e28fb520da8
Sopee Thong <tsopee@gmail.com>
2021-04-26 01:01:15 -0400

    adding .get and .post to find and create tags

--- DEVELOP/routes/api/tag-routes.js
+++ DEVELOP/routes/api/tag-routes.js
@@ -6,15 +6,45 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model:Product}],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Product.findByPk(req.params.id, {
+  
+      include: [{model:Product}],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
