750379aba4a7beb50d1864a820ffc5ee6175dcf8
Sopee Thong <tsopee@gmail.com>
2021-04-26 01:04:47 -0400

    adding.post to update tag by id

--- DEVELOP/routes/api/tag-routes.js
+++ DEVELOP/routes/api/tag-routes.js
@@ -49,6 +49,20 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
