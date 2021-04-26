a96acef09d5a4baa3df72f5a0cb33ec1c0f620a8
Sopee Thong <tsopee@gmail.com>
2021-04-26 02:42:37 -0400

    syntax error fixed

--- DEVELOP/models/Product.js
+++ DEVELOP/models/Product.js
@@ -1,46 +1,46 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require("sequelize");
-// const { Category } = require('.');
-// import our database connection from config.js
-const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
+  
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
+   
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL, 
       allowNull: false,
       validate: {
-        isDecimal: true,
-      },
+        isDecimal: true
+      }
     },
+    
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: false, 
       defaultValue: 10,
       validate: {
-        isNumeric: true,
+        isNumeric: true
       },
     },
+   
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "category",
-        key: "id"
-      }
+        model: 'category',
+        key: 'id'
+      },
     },
   },
   {
@@ -48,7 +48,8 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "product",
+    modelName: 'product',
   }
 );
+
 module.exports = Product;
--- DEVELOP/models/index.js
+++ DEVELOP/models/index.js
@@ -20,7 +20,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: "product_tag",
+
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -29,7 +29,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: "product_tag",
+
 });
 
 module.exports = {
