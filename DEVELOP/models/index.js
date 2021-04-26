df297ad055d5a26e4dd3362f04c06ffe92b40ad7
Sopee Thong <tsopee@gmail.com>
2021-04-26 00:02:20 -0400

    formatted syntax

--- DEVELOP/models/index.js
+++ DEVELOP/models/index.js
@@ -15,11 +15,23 @@ onDelete: 'CASCADE'
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{through: ProductTag});
+Product.belongsToMany(Tag,{
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag});
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag'
+});
 
 module.exports = {
   Product,
