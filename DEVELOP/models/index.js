59ba2d9b1d421c36f2151ac3bc431d7940c71e0d
Sopee Thong <tsopee@gmail.com>
2021-04-25 23:45:55 -0400

    adding Category associate

--- DEVELOP/models/index.js
+++ DEVELOP/models/index.js
@@ -5,10 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category,{
+Product.belongsTo(Category,{
 foreignKey: 'category_id'
 })
 // Categories have many Products
+Category.hasMany(Products,{
+foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
 
